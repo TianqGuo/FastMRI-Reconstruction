9d37996a2435780e682abcf9c5e2edddee22e9d9
YawenXXX <109942054+YawenXXX@users.noreply.github.com>
2024-04-20 19:12:41 -0700

    unet test

--- Fastmri_unet_dev/Testing/unet_test.py
+++ Fastmri_unet_dev/Testing/unet_test.py
@@ -1,4 +1,8 @@
 import torch
+import torch.nn
+import numpy as np
+
+from collections import defaultdict
 
 
 def test(args, model, test_loader):
@@ -8,15 +12,27 @@ def test(args, model, test_loader):
         model: pretrained model used to reconstruct MR image
         test_loader: test data loader
 
-    Returns: MRI reconstruction
+    Returns:
+        reconstruction: Dictionary of mapping from file name to reconstruction image
     '''
 
     model.eval()
-
+    reconstructions = defaultdict(list)              # defaultdict could hold a key-value pair
 
     with torch.no_grad():
-        for ... in test_loader:
+        for i, (input, _, _, fnames, slices) in enumerate(test_loader):
 
+            input = input.to(args.device)
+            pred = model(input)
 
+            N = len(fnames)                     # batch size N
+            for i in range(N):
+                reconstructions[fnames[i]].append((slices[i].numpy(), pred[i].numpy()))         # torch tensor needs to be converted to nd.array
+
+    for fname, slice_pred in reconstructions.items():
+        reconstructions[fname] = np.stack(
+            [pred for _, pred in sorted(slice_pred)]
+        )
 
     return reconstructions
+
